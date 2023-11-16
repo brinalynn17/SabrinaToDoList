96e758c384b40e15c1a4d1b032ecf683d68c6305
Sabrina Hill <brinalynn@hotmail.com>
2023-11-16 01:31:27 +0000

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,87 @@
-import { useState } from "react"
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-const [todo, setTodo] = useState([
-  {id:1, //Key
-  task: "Wish List", //string
-completed: false, //boolean
-},
-]);
+  const [todos, setTodos] = useState([
+    {
+      id: 1, //Key
+      task: "", //string
+      done: false, //boolean
+    },
+  ]);
+  const [newToDo, setNewTodo] = useState("");
 
-const [newWishItem, setWishItem] = useState("")
+  const addTodo = (task) => {
+    if (!task) return;
+    const newTodoList = [
+      ...todos,
+      { id: todos.length + 1, task: task, done: false },
+    ];
 
-const addWishItem = (task) => {
-  const newWishListItem = [
-    ...todo,
-    {id:todo.length + 1, task:task, completed: false},
-  ];
-  setTodo(newWishListItem)
-  }
+    setTodos(newTodoList);
+  };
 
-return (
+  const delteTodo = (id) => {
+    const newTodoList = todos.filter((item) => item.id != id);
+    setTodos(newTodoList);
+  };
+
+  const changeTodoState = (id, state) => {
+    const newTodoList = todos.map((item) => {
+      if (item.id === id) {
+        return { ...item, done: state };
+      } else {
+        return item;
+      }
+    });
+
+    setTodos(newTodoList);
+  };
+
+  return (
     <div className="container">
-      <h1>Sabrina's Wish List
-      </h1>
-      
-      <div className="new-wish-list-container">
-        <input 
-        value={newToDo} 
-        onChange={event=> setWishItem(event.target.value)} 
+      <h1>Sabrina's Wish List</h1>
+
+      <form className="new-todo-container" onSubmit={(e) => e.preventDefault()}>
+        <input
+          value={newToDo}
+          onChange={(event) => setNewTodo(event.target.value)}
         />
-
-        <button 
-        onClick={()=>{
-          addToDo(newToDo);
-          setNewToDo("")
-          }}>
+        <button
+          onClick={() => {
+            addTodo(newToDo);
+            setNewTodo("");
+          }}
+        >
           ➕
-          </button>
-      </div>
+        </button>
+      </form>
 
-      <ul className="wish-list">           
-      {todo.map((item) =>{
-        return(
-          <li key= {item.id} className="wish-list-items">
-          <input type="checkbox" value={item.completed} />
-          <span>{item.task}</span>
-          <button className="delete">❌</button>
-          </li>
-        )
-      })}
+      {todos.length === 0 ? (
+      <p>No tasks, add a task</p>
+    ) : (
 
+      <ul className="todo-list">
+        {todos.map((item) => {
+          return (
+            <li key={item.id} className="todo-item">
+              <input
+                type="checkbox"
+                value={item.done}
+                className={item.done ? "done" : ""}
+                onChange={(e) => changeTodoState(item.id, e.target.checked)}
+              />
+              <span className="todo-item-text">{item.task}</span>
+              <button onClick={() => delteTodo(item.id)} className="delete">
+                ❌
+              </button>
+            </li>
+          );
+        })}
       </ul>
+    )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
